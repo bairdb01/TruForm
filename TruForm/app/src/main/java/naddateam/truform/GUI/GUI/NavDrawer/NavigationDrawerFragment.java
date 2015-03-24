30d1414ce6f624552b4a094fa0255e0bc4ce467a
Andrew Huynh <ahuynh@mail.uoguelph.ca>
2015-03-24 19:10:25 -0400

    Coloured backgrounds and a few texts. Need colours in listviews and actions bars.

--- TruForm/app/src/main/java/naddateam/truform/GUI/GUI/NavDrawer/NavigationDrawerFragment.java
+++ TruForm/app/src/main/java/naddateam/truform/GUI/GUI/NavDrawer/NavigationDrawerFragment.java
@@ -247,7 +247,7 @@ public class NavigationDrawerFragment extends Fragment {
         // showGlobalContextActionBar, which controls the top-left area of the action bar.
         if (mDrawerLayout != null && isDrawerOpen()) {
             inflater.inflate(R.menu.global, menu);
-            showGlobalContextActionBar();
+            //showGlobalContextActionBar();
         }
         super.onCreateOptionsMenu(menu, inflater);
     }
@@ -257,12 +257,12 @@ public class NavigationDrawerFragment extends Fragment {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
+/*
         if (item.getItemId() == R.id.action_example) {
             Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
-
+*/
         return super.onOptionsItemSelected(item);
     }
 
