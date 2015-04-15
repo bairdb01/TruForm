2e6ea35b00855801dc2fb25a4921c3e33dca7ab8
Erik <erik_hoffman@outlook.com>
2015-04-14 22:43:32 -0400

    Weird paranthesis

--- TruForm/app/src/main/java/naddateam/truform/functionality/ExerciseAnalysis.java
+++ TruForm/app/src/main/java/naddateam/truform/functionality/ExerciseAnalysis.java
@@ -260,11 +260,9 @@ public class ExerciseAnalysis {
         {
             gyroZ = (-1)*gyroZArr.get(i);
             accelY = accelYArr.get(i);
-            accelZ = accelZArr.get(i);
             if(i == 0)
             {
                 startingAccelY = accelY;
-                startingAccelZ = accelZ;
             }
 
             if((totalGyroZDown < 0) && (accelY > (startingAccelY - 5)) && (accelY < (startingAccelY + 5))) /*You've reached the bottom of the rep*/
@@ -320,7 +318,7 @@ public class ExerciseAnalysis {
                     sittingBottom = 1; /*Now you are sitting at the bottom*/
                     //if(totalGyroZDown < 0) /*You're at the bottom of the curl and it hasn't increased the rep yet
 				//subsequent zeroes found on new reps will not trigger a new rep, only the first one*/
-                    if((accelY > (startingAccelY - 5)) && (accelY < (startingAccelY + 5) && (totalGyroZDown < 0))){
+                    if((accelY > (startingAccelY - 5)) && (accelY < (startingAccelY + 5)) && (totalGyroZDown < 0)){
                     /*Comparing the accelerometer values to see if you've reached the starting point again*/
                         //numZeroes[j] += 1;
                         zeroes += 1;
